89bc9647351e74dd42eba08cca496e27e7cf8ed7
Your Full Name <your.email@example.com>
2025-05-27 12:17:42 +0300

    first commit

---
+++ bin/project_name.dart
@@ -0,0 +1,5 @@
+import 'package:project_name/project_name.dart' as project_name;
+
+void main(List<String> arguments) {
+  print('Hello world: ${project_name.calculate()}!');
+}
---
+++ test/project_name_test.dart
@@ -0,0 +1,8 @@
+import 'package:project_name/project_name.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
