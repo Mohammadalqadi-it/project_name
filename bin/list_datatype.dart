274ca008c899d47c52b5d2a21230e88184f51b85
Your Full Name <your.email@example.com>
2025-05-28 14:22:51 +0300

    list datatype

---
+++ bin/list_datatype.dart
@@ -0,0 +1,28 @@
+void main() {
+  //private variable
+  List _list = [true, 1, 1.5, "", ''];
+
+  List<num> numbers = [5.6, 6, 1, -1];
+
+  List<bool> boolList = [false, true, false];
+
+  List<String> myList = ["5", '6', '7', "ckdbck", "true", "5.5"];
+
+  // print(myList.length);
+
+  // print(myList.first);
+
+  // print(myList.last);
+
+  // for (var element in myList) {
+  //   print(element);
+  // }
+
+  // myList.map((e) => print(e));
+
+  // for (int i = 0; i < myList.length; i++) {
+  //   print(myList[i]);
+  // }
+
+  //==========================================
+}
