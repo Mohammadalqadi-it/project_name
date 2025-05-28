e080d255c3cc088c29b1ce86b69c4b9fbacb3e4f
Your Full Name <your.email@example.com>
2025-05-28 14:24:47 +0300

    siwtch

---
+++ bin/switch_statment.dart
@@ -0,0 +1,31 @@
+void main() {
+  int mark = 51;
+
+  switch (mark) {
+    case < 50:
+      print("Fail");
+      break;
+    case >= 50 && < 60:
+      print("D");
+      break;
+    case > 1:
+      print("C");
+      break;
+    case >= 70 && < 80:
+      print("B");
+      break;
+    case >= 80 && < 90:
+      print("A");
+      break;
+    case > 90:
+      print("A+");
+      break;
+    default:
+  }
+}
+
+//ctrl + D
+
+//ctrl + Z
+
+//ctrl + Y
