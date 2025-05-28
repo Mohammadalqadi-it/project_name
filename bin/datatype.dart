3ee78faa5a4bb6680c749748e39c247a55d2c957
Your Full Name <your.email@example.com>
2025-05-28 14:22:29 +0300

    data type

---
+++ bin/datatype.dart
@@ -0,0 +1,30 @@
+void main() {
+  // [+] operation
+  print(5 + 5);
+
+  // [*] operation
+  print(5 * 5);
+
+  // [/] operaton
+  print(5 / 5);
+  // [%] (mod) operation
+  print(10 % 9);
+
+  //[String] datatype
+  String myStringForSomething = 'string datatype';
+
+  // [int] datatype
+  int intNumber = 5;
+
+  // [double] datatype
+  double doubleNumber = 5.5;
+
+  // [num] datatype: int can not containe [double], and double can not contain [int]
+  num numForAll = 5;
+
+  // [num] can contain [double] or [int]
+  num numForAll2 = 5.5;
+
+  //[bool] datatype
+  bool flag = true;
+}
